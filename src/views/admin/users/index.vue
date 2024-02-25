642cd4f5ef71f6e012837fbe2c9f42888c3458e2
Normal <thuonghuunguyen2002@gmail.com>
2024-02-26 06:37:10 +0700

    change axios request

--- src/views/admin/users/index.vue
+++ src/views/admin/users/index.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-12 d-flex justify-content-end align-items-center my-3">
         <router-link :to="{ name: 'admin-users-add' }">
-          <BtnCreate />
+          <BtnCreate/>
         </router-link>
       </div>
     </div>
@@ -13,10 +13,10 @@
           <template #bodyCell="{ column }">
             <template v-if="column.key === 'action'">
               <router-link :to="{ name: 'admin-users-edit' }">
-                <BtnEdit />
+                <BtnEdit/>
               </router-link>
 
-              <BtnDel />
+              <BtnDel/>
             </template>
           </template>
         </a-table>
@@ -25,13 +25,13 @@
   </a-card>
 </template>
 <script>
-import { PlusOutlined, EditOutlined } from "@ant-design/icons-vue";
+import {PlusOutlined, EditOutlined} from "@ant-design/icons-vue";
 import BtnCreate from "../../../components/BtnCreate.vue";
 import BtnEdit from "../../../components/BtnEdit.vue";
 import BtnDel from "../../../components/BtnDel.vue";
 
-import { defineComponent, ref } from "vue";
-import { useMenu } from "../../../store/useMenu.js";
+import {defineComponent, ref} from "vue";
+import {useMenu} from "../../../store/useMenu.js";
 
 export default {
   components: {
@@ -51,49 +51,65 @@ export default {
         key: "id",
       },
       {
-        title: "Tài khoản",
-        dataIndex: "username",
-        key: "username",
+        title: "Họ",
+        dataIndex: "first_name",
+        key: "first_name",
       },
       {
-        title: "Mật khẩu",
-        dataIndex: "password",
-        key: "password",
+        title: "Tên",
+        dataIndex: "last_name",
+        key: "last_name",
+      },
+      {
+        title: "Birthday",
+        dataIndex: "birth_date",
+        key: "birth_date",
+      },
+      {
+        title: "Giới tính",
+        dataIndex: "gender",
+        key: "gender",
         // Đặt hide để ẩn cột mật khẩu khỏi bảng
         hide: true,
-      },
-      {
-        title: "Email",
-        dataIndex: "email",
-        key: "email",
-      },
-      {
-        title: "Họ và tên",
-        dataIndex: "fullname",
-        key: "fullname",
-      },
-      {
-        title: "Vai trò",
-        dataIndex: "role",
-        key: "role",
-      },
-      {
-        title: "Thao tác",
-        dataIndex: "action",
-        key: "action",
-        fixed: "right",
-      },
+      }
+      // ,
+      // {
+      //   title: "Email",
+      //   dataIndex: "email",
+      //   key: "email",
+      // }
+      // // ,
+      // {
+      //   title: "Vai trò",
+      //   dataIndex: "role",
+      //   key: "role",
+      // },
+      // {
+      //   title: "Thao tác",
+      //   dataIndex: "action",
+      //   key: "action",
+      //   fixed: "right",
+      // },
     ];
     const getUsers = () => {
       axios
-        .get("http://127.0.0.1:5173/user.json")
-        .then(function (response) {
-          users.value = response.data;
-        })
-        .catch(function (error) {
-          // handle error
-          console.log(error);
-        });
+          .put("http://127.0.0.1:2002/api/student-put/5 ",
+              {
+                "first_name": "áda",
+                "last_name": "bro"
+              }, {
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              }
+          )
+          .then(function (response) {
+            users.value = response.data;
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          });
     };
 
     getUsers();
